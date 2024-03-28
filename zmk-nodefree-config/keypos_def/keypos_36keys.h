443fc7f5d1f06a6d88e35a63b5785f9514be3d90
diologix <62773168+diologix@users.noreply.github.com>
2024-03-28 19:32:09 +0100

    Add files via upload

--- zmk-nodefree-config/keypos_def/keypos_36keys.h
+++ zmk-nodefree-config/keypos_def/keypos_36keys.h
@@ -1,11 +1,11 @@
-/*                              34 KEY MATRIX / LAYOUT MAPPING
+/*                              36 KEY MATRIX / LAYOUT MAPPING
 
   ╭────────────────────┬────────────────────╮ ╭─────────────────────┬─────────────────────╮
   │  0   1   2   3   4 │  5   6   7   8   9 │ │ LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 │
   │ 10  11  12  13  14 │ 15  16  17  18  19 │ │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
   │ 20  21  22  23  24 │ 25  26  27  28  29 │ │ LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 │
-  ╰───────────╮ 30  31 │ 32  33 ╭───────────╯ ╰───────────╮ LH1 LH0 │ RH0 RH1 ╭───────────╯
-              ╰────────┴────────╯                         ╰─────────┴─────────╯             */
+  ╰───────╮ 30  31  32 │ 33  34  35 ╭───────╯ ╰───────╮ LH2 LH1 LH0 │ RH0 RH1 RH2 ╭───────╯
+          ╰────────────┴────────────╯                 ╰─────────────┴─────────────╯             */
 
 #pragma once
 
@@ -45,9 +45,11 @@
 #define RB3 28
 #define RB4 29
 
-#define LH0 31  // left thumb keys
-#define LH1 30
+#define LH0 32  // left thumb keys
+#define LH1 31
+#define LH2 30
 
-#define RH0 32  // right thumb keys
-#define RH1 33
+#define RH0 33  // right thumb keys
+#define RH1 34
+#define RH2 35
 
